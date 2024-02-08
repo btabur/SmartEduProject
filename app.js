c8d9d5b58cc1ca4d37048965c251f3935e4ba685
btabur <webtasarim.0323@gmail.com>
2024-02-08 12:05:52 +0300

    lesson04

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const pageRoute = require('./routes/pageRoute')
+const mongoose = require('mongoose')
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute')
+
 
 const app = express();
+
+//connetct db
+
+mongoose.connect('mongodb://127.0.0.1/smartEdu-db')
+.then(()=> console.log('Db Connected Succesfuly'));
 
 
 //Template Engine
@@ -13,6 +21,7 @@ app.use(express.static("public"))
 
 //Routers
 app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 
 const port = 3000;
