369f6eff4216668be4980ebeb76df0e088736f96
btabur <webtasarim.0323@gmail.com>
2024-02-08 21:07:03 +0300

    created course single page and used slugify

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema({
@@ -15,8 +16,21 @@ const CourseSchema = new Schema({
     createdAt:{
         type:Date,
         default:Date.now
+    },
+    slug:{
+        type:String,
+        unique:true,
     }
 
+});
+
+//veri tabanına kaydedilmeden önce kullanılan kurs ismini slugify ile slug şekline çevirerek modele ekler
+CourseSchema.pre('validate',function(next){
+    this.slug=slugify(this.name, {
+        lover:true,
+        strict:true, // name içerisinde : gibi gereksiz karakterler varsa bunları yok sayacak
+    })
+    next();
 })
 
 const Course = mongoose.model('Course',CourseSchema);
