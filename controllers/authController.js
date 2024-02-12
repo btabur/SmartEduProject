4dd110898df97c128878818a2e6c350e9841932c
btabur <webtasarim.0323@gmail.com>
2024-02-12 13:43:51 +0300

    login operations have been completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -8,6 +9,30 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.loginUser = (req, res) => {
+  try {
+    const { email, password } = req.body;
+    User.findOne({ email })
+      .then((response) => {
+        bcrypt.compare(password, response.password)
+          .then(() => {
+            res.status(200).send("You are login");
+          })
+          .catch(() => {
+            res.status(200).send("Password is not true");
+          });
+      })
+      .catch(() => {
+        res.status(200).send("User was not founded");
+      });
   } catch (error) {
     res.status(400).json({
       status: "fail",
