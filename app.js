b9385b70a60d0f3674020b5d9948c5a338822091
btabur <webtasarim.0323@gmail.com>
2024-02-13 13:10:31 +0300

    logged out and connect-mongo added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const session = require('express-session')
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -30,7 +31,8 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.use(session({
   secret: 'my_keyboard_cat',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1/smartEdu-db' })
 }))
 
 //Routers
--- controllers/authController.js
+++ controllers/authController.js
@@ -46,3 +46,9 @@ exports.loginUser = (req, res) => {
     });
   }
 };
+
+exports.logoutUser = (req,res)=> {
+  req.session.destroy(()=> {
+    res.redirect('/')
+  })
+}
