f63d6f9b5c2df5e52a02d99156b8ff2cab7131e1
btabur <webtasarim.0323@gmail.com>
2024-02-16 15:17:52 +0300

    Relationship between course and student updated

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -56,11 +56,13 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
+    const user = await User.findById(req.session.userID)
     const course = await Course.findOne({slug: req.params.slug}).populate('user')  //user bilgisini eklemek için bu şekilde kullandık
 
     res.status(200).render("course", {
       course,
       page_name: "courses",
+      user
     });
   } catch (error) {
     res.status(400).json({
@@ -74,6 +76,21 @@ exports.enrollCourse = async (req, res) => {
   try {
    const user = await User.findById(req.session.userID);
    await user.courses.push({_id: req.body.course_id});
+   await user.save();
+
+   res.status(200).redirect('/users/dashboard')
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.releaseCourse = async (req, res) => {
+  try {
+   const user = await User.findById(req.session.userID);
+   await user.courses.pull({_id: req.body.course_id});
    await user.save();
 
    res.status(200).redirect('/users/dashboard')
